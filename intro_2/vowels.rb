917410f6028e769c000737493447552c81ab13f0
Andres Peterson <andres_p@mail.ru>
2017-06-04 18:55:11 +0300

    ispravil

--- intro_2/vowels.rb
+++ intro_2/vowels.rb
@@ -2,12 +2,9 @@ vowels = %w[a e i o u y]
 all = ("a".."z").to_a
 hash = {}
 
-i = 0
-
 vowels.each do |l|
   if all.include? l
-    hash["#{vowels[i]}"] = "#{all.index(l)+1}"
-    i += 1
+    hash[l] = all.index(l) + 1
   end
 end
 puts hash
