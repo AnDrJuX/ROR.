83e32841cc641f4fc758baa5bf35a72fb9fd6a18
Andres Peterson <andres_p@mail.ru>
2017-07-29 08:06:57 +0300

    3

--- Tutu/Tutu/db/migrate/20170726053611_add_route_id_to_tickets.rb
+++
@@ -1,4 +0,0 @@
-class AddRouteIdToTickets < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
--- Tutu/Tutu/db/schema.rb
+++ Tutu/Tutu/db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170725054123) do
+ActiveRecord::Schema.define(version: 20170726054312) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "railway_stations_routes", force: :cascade do |t|
+    t.integer "railway_station_id"
+    t.integer "route_id"
   end
 
   create_table "routes", force: :cascade do |t|
@@ -25,12 +30,20 @@ ActiveRecord::Schema.define(version: 20170725054123) do
   create_table "tickets", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "routes_id"
+    t.integer "current_station_id"
+    t.index ["current_station_id"], name: "index_tickets_on_current_station_id"
+    t.index ["routes_id"], name: "index_tickets_on_routes_id"
   end
 
   create_table "trains", force: :cascade do |t|
     t.string "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "current_station_id"
+    t.integer "route_id"
+    t.index ["current_station_id"], name: "index_trains_on_current_station_id"
+    t.index ["route_id"], name: "index_trains_on_route_id"
   end
 
   create_table "users", force: :cascade do |t|
